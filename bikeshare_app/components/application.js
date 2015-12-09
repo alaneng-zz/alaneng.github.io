32b6a37d7230128c229fffdcfd4484e4cb945aeb
Alan Eng <aeng@stitchfix.com>
2015-12-09 11:34:32 -0800

    Remove router from requirement

--- bikeshare_app/components/application.js
+++ bikeshare_app/components/application.js
@@ -1,9 +1,6 @@
 // Component declarations
 var React = require("react");
 var ReactDOM = require("react-dom");
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
 
 
 var Navbar = require("react-bootstrap/lib/Navbar");
