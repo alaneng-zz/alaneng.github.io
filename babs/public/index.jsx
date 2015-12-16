c31e2684b0f9bc184eda2759877169af7a206b55
Alan Eng <aeng@stitchfix.com>
2015-12-16 15:12:14 -0800

    Rename babs to github

--- babs/public/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Router, { Route } from 'react-router';
-import app from '../src/components/app.jsx'
-import routes from '../src/config/routes.jsx'
-
-ReactDOM.render(<Router>{routes}</Router>, document.getElementById('root')) 
--- babs/src/config/routes.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import Router, { Route } from 'react-router';
-import App from '../components/app.jsx'
-
-module.exports = (
-  <Route path='/' component={App}>
-  </Route>
-)
