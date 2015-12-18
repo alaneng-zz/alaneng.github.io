1cd25a4225f1126c8e246d5af81a9a6db77e9359
Alan Eng <aeng@stitchfix.com>
2015-12-18 00:49:23 -0800

    Update webpack prod config

--- github_userlookup/webpack.production.config.js
+++ github_userlookup/webpack.production.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: "./public/index.jsx",
   output: {
     path: path.join(__dirname),
     filename: 'bundle.js'
@@ -10,19 +10,32 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        include: path.join(__dirname, 'src'),
-        loaders: ['babel?stage=0'],
+        test: /\.jsx$/,
+        include: path.join(__dirname, 'src/config'),
+        loaders: ['react-hot', 'babel?stage=0'],
       },
       {
         test: /\.jsx$/,
-        include: path.join(__dirname, 'src'),
-        loaders: ['babel?stage=0'],
+        include: path.join(__dirname, 'src/components'),
+        loaders: ['react-hot', 'babel?stage=0'],
       },
       {
-       test: /\.scss$/,
-       loaders: ['style', 'css', 'sass']
+        test: /\.jsx$/,
+        include: path.join(__dirname, 'src/utils'),
+        loaders: ['react-hot', 'babel?stage=0'],
       },
+      {
+        test: /\.jsx$/,
+        include: path.join(__dirname, 'public'),
+        loaders: ['react-hot', 'babel?stage=0'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
