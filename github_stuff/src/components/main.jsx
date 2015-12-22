5a1a24fd0a09f38f7e077fb12567dace7cb36215
Alan Eng <aeng@stitchfix.com>
2015-12-21 20:20:29 -0800

    Remove Navbar

--- github_stuff/src/components/main.jsx
+++ github_stuff/src/components/main.jsx
@@ -11,16 +11,6 @@ var Main = React.createClass({
   render() {
     return (
       <Grid>
-        {/*<Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Github Profiler</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem>Menu</NavItem>
-          </Nav>
-        </Navbar>*/}
         <Jumbotron>
           <h1><Link to="/" style={{color: 'black', textDecoration: 'none'}}>Github Profiler</Link></h1>
           <SearchGithub />
