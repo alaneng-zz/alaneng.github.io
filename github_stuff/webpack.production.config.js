b80c5d6161872935eb9f849dcb88005963326f56
Alan Eng <aeng@stitchfix.com>
2015-12-22 00:13:28 -0800

    test

--- github_stuff/webpack.production.config.js
+++ github_stuff/webpack.production.config.js
@@ -12,25 +12,30 @@ module.exports = {
       {
         test: /\.jsx$/,
         include: path.join(__dirname, 'src/config'),
+        exclude: /node_modules/,
         loaders: ['react-hot', 'babel?stage=0'],
       },
       {
         test: /\.jsx$/,
         include: path.join(__dirname, 'src/components'),
+        exclude: /node_modules/,
         loaders: ['react-hot', 'babel?stage=0'],
       },
       {
         test: /\.jsx$/,
         include: path.join(__dirname, 'src/utils'),
+        exclude: /node_modules/,
         loaders: ['react-hot', 'babel?stage=0'],
       },
       {
         test: /\.jsx$/,
         include: path.join(__dirname, 'public'),
+        exclude: /node_modules/,
         loaders: ['react-hot', 'babel?stage=0'],
       },
       {
         test: /\.scss$/,
+        exclude: /node_modules/,
         loaders: ['style', 'css', 'sass']
       }
     ]
