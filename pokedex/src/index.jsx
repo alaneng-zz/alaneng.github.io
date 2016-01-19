8647d53b9c3015e3c0d5363adc84383a1347b84a
Alan Eng <aeng@stitchfix.com>
2016-01-18 22:49:48 -0800

    remove pokedex

--- pokedex/src/index.jsx
+++
@@ -1,7 +0,0 @@
-require('./style.scss')
-
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import App from './app.jsx'
-
-ReactDOM.render(<App />, document.getElementById('root'))
