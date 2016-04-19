c61afb7fab0cdee16b1b1e5b4994d21351c97eb4
Alan Eng <aeng@stitchfix.com>
2016-04-19 00:08:16 -0700

    New layout design using flexbox

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,10 +7,26 @@ class Homepage extends React.Component {
 
   render() {
     return <div className='homepage'>
-             <div className='homepage-child'>child1</div>
-             <div className='homepage-child'>child2</div>
-             <div className='homepage-child'>child3</div>
-             <div className='homepage-child'>child4</div>
+
+            <section className='header'>
+                <h1>alan eng</h1>
+            </section>
+
+            <section className='sections'>
+              <div className='box'>
+                me
+              </div>
+              <div className='secondary box'>
+                stitch fix
+              </div>
+              <div className='box'>
+                projects
+              </div>
+              <div className='box'>
+                contact
+              </div>
+            </section>
+
            </div>
   }
 };
