7608bec95f86d8dcd907fd75958daebca26da85d
Alan Eng <aeng@stitchfix.com>
2016-08-04 11:27:13 -0700

    Add settlers 2

---
+++ settlers2/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
