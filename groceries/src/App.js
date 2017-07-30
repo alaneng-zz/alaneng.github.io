f26f5ca312dd4252e02b25a3ffedab1b07c7c33e
Alan Eng <aeng@stitchfix.com>
2017-07-29 21:30:10 -0700

    CSS centering

--- groceries/src/App.js
+++ groceries/src/App.js
@@ -9,9 +9,14 @@ import {
   fetchGroceryList,
   addNewItemType,
 } from "./actions"
+import { foodTypeOptions } from "./utils"
 import "./App.css"
 
 class App extends Component {
+  state = {
+    dropdownOpen: false,
+  }
+
   componentWillMount() {
     this.props.fetchGroceryList()
   }
@@ -24,12 +29,11 @@ class App extends Component {
 
   render() {
     const { groceryList } = this.props
-    console.log(this.props.groceryList)
 
     const groceryCards = _.keys(groceryList).map(itemType => {
       const groceryItems = _.values(groceryList[itemType])
       const groceryListItems = groceryItems.map(item =>
-        <List.Item key={item}>
+        <List.Item key={item} className="grocery-item">
           {item}
         </List.Item>
       )
@@ -53,39 +57,43 @@ class App extends Component {
       )
     })
 
-    const meat = "meat"
-    const vegetables = "vegetables"
-    const drink = "drink"
-    const fruit = "fruit"
-
-    const foodTypeOptions = [
-      { key: meat, text: meat, value: meat },
-      { key: vegetables, text: vegetables, value: vegetables },
-      { key: drink, text: drink, value: drink },
-      { key: fruit, text: fruit, value: fruit },
-    ]
+    const addNewItemIsDisabled =
+      !this.props.inputGroceryItem || !this.props.inputGroceryItemType
 
     return (
-      <div className="App">
-        <Input
-          label={
-            <Dropdown
-              text={this.props.inputGroceryItemType || "select type..."}
-              options={foodTypeOptions}
-              onChange={(e, data) => this.props.addNewItemType(data.value)}
-            />
-          }
-          labelPosition="right"
-          placeholder="enter grocery item..."
-          value={this.props.inputGroceryItem}
-          onChange={(event, data) => this.props.onEnterGroceryItem(data)}
-          onKeyPress={e => this.handleKeyPress(e)}
-        />
-        <Button circular basic onClick={() => this.props.addNewItem()}>
-          Add new item
-        </Button>
-        <div className="grocery-cards">
-          {groceryCards}
+      <div>
+        <div className="item-input">
+          <Input
+            style={{ userSelect: "none" }}
+            label={
+              <Dropdown
+                //                open={this.state.dropdownOpen}
+                text={this.props.inputGroceryItemType || "select type..."}
+                options={foodTypeOptions}
+                onChange={(e, data) => this.props.addNewItemType(data.value)}
+                //                onMouseDown={() => this.setState({ dropdownOpen: true })}
+                //                onMouseUp={() => this.setState({ dropdownOpen: false })}
+              />
+            }
+            labelPosition="right"
+            placeholder="enter grocery item..."
+            value={this.props.inputGroceryItem}
+            onChange={(event, data) => this.props.onEnterGroceryItem(data)}
+            onKeyPress={e => this.handleKeyPress(e)}
+          />
+          <Button
+            circular
+            basic
+            onClick={() => this.props.addNewItem()}
+            disabled={addNewItemIsDisabled}
+          >
+            Add new item
+          </Button>
+        </div>
+        <div className="grocery-container">
+          <div className="grocery-cards">
+            {groceryCards}
+          </div>
         </div>
       </div>
     )
---
+++ groceries/src/utils/index.js
@@ -0,0 +1,17 @@
+const meat = "meat"
+const vegetables = "vegetables"
+const drink = "drink"
+const fruit = "fruit"
+const snacks = "snacks"
+const condiments = "condiments"
+const pastries = "pastries"
+
+export const foodTypeOptions = [
+  { key: meat, text: meat, value: meat },
+  { key: vegetables, text: vegetables, value: vegetables },
+  { key: drink, text: drink, value: drink },
+  { key: fruit, text: fruit, value: fruit },
+  { key: snacks, text: snacks, value: snacks },
+  { key: condiments, text: condiments, value: condiments },
+  { key: pastries, text: pastries, value: pastries },
+]
